360f53940773da72c92fa156b466c03ff63f3db6
Jillian Ocasio <jillianocasio@gmail.com>
2018-01-22 20:15:45 -0500

    Calculate accuracy and setup for questions

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -23,9 +23,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+def classify(features_train, labels_train,split_number):
+    from sklearn import tree
+    clf = tree.DecisionTreeClassifier(min_samples_split=split_number)
+    clf = clf.fit(features_train, labels_train)
+    
+    
+    return clf
+    
+def accuracy(features_train, labels_train, features_test, labels_test, split_number):
+    clf=classify(features_train, labels_train,split_number)
+    pred=clf.predict(features_test)
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    return acc 
 
-
+split_number=40
+acc_min_samples_split=accuracy(features_train, labels_train, features_test, labels_test, split_number)
+print acc_min_samples_split
+total_features=len(features_train)+len(features_test)
+print {"Total features": round(total_features)}
 #########################################################
 
 
